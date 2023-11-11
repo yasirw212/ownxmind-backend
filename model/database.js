dc3a8e25f97a711a71be4e3d00add83b8b201f8a
yasirw212 <williamsyasir90@gmail.com>
2023-11-10 21:41:42 -0600

    Update database.js

--- model/database.js
+++ model/database.js
@@ -3,9 +3,11 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'me',
     database: 'ownxmind',
-    host: 'localhost',
-    password: 'password',
+    host: 'dpg-cl7eku76e7vc739s6630-a',
+    password: 'gxQFvhvaTfIMpLd0OSTADsp2kLnA9agd',
     port: 5432
 })
+                                                                                                                                                                   
+postgres://me:gxQFvhvaTfIMpLd0OSTADsp2kLnA9agd@dpg-cl7eku76e7vc739s6630-a.oregon-postgres.render.com/ownxmind
 
 module.exports = pool
