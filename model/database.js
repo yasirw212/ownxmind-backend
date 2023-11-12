4a827164f4c4de32baa7efd0c8ee1fe8a8d9dfe4
yasirw212 <williamsyasir90@gmail.com>
2023-11-11 19:43:04 -0600

    establish db connection

--- model/database.js
+++ model/database.js
@@ -5,7 +5,9 @@ const client = new Client({
     connectionString: 'postgres://me:gxQFvhvaTfIMpLd0OSTADsp2kLnA9agd@dpg-cl7eku76e7vc739s6630-a.oregon-postgres.render.com/ownxmind?ssl=true'
 })
 
-client.connect()
+setInterval(() => {
+    client.connect()
+}, 50000)
 
 
 // const pool = new Pool({
