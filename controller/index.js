30b7762e97a6310e59d9dc3e4b08ff8b8af82ce2
yasirw212 <williamsyasir90@gmail.com>
2024-01-03 11:26:44 -0600

    Database call to receive details

--- controller/index.js
+++ controller/index.js
@@ -11,14 +11,24 @@ const getProducts = async (req, res) =>{
         } else {
             try {
                 const {data} = await supaBase.from('products_sizes').select('*')
-                
                 for(let i = 0; i < productsData.length; i++){
                     let sizes = data.filter(size => size.product_id == productsData[i].id)
                     productsData[i] = { ...productsData[i], sizes: sizes}
                 }
-                return res.json(productsData)
+                
             } catch (error) {
                 console.log(error)
+            }
+            if(!data){
+                throw new Error('failed')
+            } else {
+                const {data} = await supaBase.from('details').select('*')
+                for(let i = 0; i < productsData.length; i++){
+                    let details = data.find(detail => detail.id == productsData[i].details_id)
+                    productsData[i] = { ...productsData[i], details: details.details}
+                }
+                console.log(productsData)
+                return res.json(productsData)
             }
         }
         
